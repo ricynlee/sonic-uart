cb44ae033d3029ad8d920cbb309daa0d79b05d9d
ricynlee <ricyn@qq.com>
2023-03-30 22:08:08 +0800

    202303302208

--- tx/tx.cpp
+++ tx/tx.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-#define SAMPLES_TX_ATOMIC_UNIT 1024 // common divisor of samples per DSSS code and samples per byte (2 symbols minimum can be a byte), cannot be too small (e.g., 128) so as not to under run
+#define SAMPLES_TX_ATOMIC_UNIT 1024 // common divisor of samples per DSSS code and samples per byte (2 symbols minimum can be a byte), >ORDER, and cannot be too small (e.g., 128) so as not to under run
 
 typedef struct {
     float left;
@@ -22,13 +22,12 @@ typedef union {
     int    i[8]; // only 32-bit int supported
 } packed_t;
 
-__attribute__((aligned(4))) float b[ORDER+1];
-__attribute__((aligned(4))) float z[ORDER+1];
+__attribute__((aligned(4))) float b[(ORDER+1)/8][8];
+__attribute__((aligned(4))) float z[(ORDER+1)/8][8];
 #endif
 
-
 void init_filter() {
-    memset(z, 0, ORDER+1);
+    memset(z, 0, sizeof(z));
 #if defined(__AVX__)
 
     packed_t vindex = {
@@ -41,21 +40,35 @@ void init_filter() {
 }
 
 inline float filter(float x) {
+    static int i = 0;
 #if defined(__AVX__)
     packed_t s = {
         .f = {0}
     };
-    for (int i=0; i<(ORDER+1)/8; i++) {
-        s.d = _mm256_fmadd_ps(((packed_t*)(z+i*8))->d, ((packed_t*)(b+i*8))->d, s.d);
+
+    i = (i+sizeof(z)/sizeof(z[0])-1)%(sizeof(z)/sizeof(z[0]));
+    for (int j=0; j<7; j++) {
+        z[i][j] = z[i][j+1];
+    }
+    z[i][7] = x;
+
+    i = (i+1)%(sizeof(z)/sizeof(z[0]));
+    for (int j=0; j<(sizeof(z)/sizeof(z[0])); j++) {
+        s.d = _mm256_fmadd_ps(((packed_t*)(z+(j+i)%(sizeof(z)/sizeof(z[0]))))->d, ((packed_t*)(b+j))->d, s.d);
+    }
+    float y = 0;
+    for (int j=0; j<8; j++) {
+        y += s.f[j];
     }
 
-
-#else // fpu
+    i = (i+1)%(sizeof(z)/sizeof(z[0]));
+    return y;
+#else // non-AVX
 
 #endif
 }
 
-fifo<int16_t> itdi; // inter-thread data interface
+fifo<float> q; // inter-thread data queue
 
 int tx_callback( void* out_buf, void* /* in_buf */, unsigned /* buf_samples */, double /* timestamp */, RtAudioStreamStatus status, void* /* shared_data */) {
     if (status) cerr << "Underflow!" << endl;
@@ -63,12 +76,11 @@ int tx_callback( void* out_buf, void* /* in_buf */, unsigned /* buf_samples */, 
     static int remaining_samples = SAMPLES_TX_ATOMIC_UNIT;
 
     sample_t* buffer = (sample_t*) out_buf;
-    int16_t x;
-    int y;
+    float x;
     for (int i=0; i<SAMPLES_TX_ATOMIC_UNIT; i++) {
-        unsigned fifo_size = itdi.size();
+        unsigned fifo_size = q.size();
         if ((fifo_size >= SAMPLES_TX_ATOMIC_UNIT-i) || (fifo_size >= remaining_samples)) {
-            if (itdi.peek(x)) {
+            if (q.peek(x)) {
                 if ((--remaining_samples)==0) {
                     remaining_samples = SAMPLES_TX_ATOMIC_UNIT;
                 }
@@ -79,10 +91,8 @@ int tx_callback( void* out_buf, void* /* in_buf */, unsigned /* buf_samples */, 
             x = 0;
         }
 
-        y = tx_filter(x);
-
-        buffer[i].left = y;
-        buffer[i].right = y;
+        buffer[i].left = x;
+        buffer[i].right = x;
     }
 
     return 0;
@@ -95,23 +105,27 @@ void tx_modulate(const char* const data, unsigned len) {
     // frame header
     for (int i=0; i<CHIPS; i++) {
         for (int j=0; j<SAMPLES_PER_CHIP; j++) {
-            itdi.write(MOD_OSC_2PSK[0][j & 7] ^ (-MSEQ[i]));
+            q.write(SIN[j & 7] * filter(1-2*MSEQ[i]));
         }
     }
     // bubble for 1 chip
     for (int i=0; i<SAMPLES_PER_CHIP; i++) {
-        itdi.write(0);
+        q.write(SIN[i & 7] * filter(0));
     }
     // frame body
-    // int bit;
-    // for (const char* p=data; *p; p++) {
-    //     for (int i=0; i<8; i++) {
-    //         bit = ((*p)>>i) & 1; // lsb first
-    //         for (int j=0; j<SAMPLES_PER_BIT; j++) {
-    //             itdi.write(MOD_OSC_2PSK[bit][j & 7]);
-    //         }
-    //     }
-    // }
+    int bit;
+    for (const char* p=data; *p; p++) {
+        for (int i=0; i<8; i++) {
+            bit = ((*p)>>i) & 1; // lsb first
+            for (int j=0; j<SAMPLES_PER_BIT; j++) {
+                q.write(SIN[j & 7] * filter(1-2*bit));
+            }
+        }
+    }
+    // pick up remainders in the filter
+    for (int i=0; i<SAMPLES_TX_ATOMIC_UNIT; i++) {
+        q.write(SIN[i & 7] * filter(0));
+    }
 }
 
 void ui(void) {
@@ -140,7 +154,7 @@ int main()
     unsigned int bufferFrames = SAMPLES_TX_ATOMIC_UNIT;
 
     try {
-        dev.openStream(&parameters, NULL, RTAUDIO_SINT16, sampleRate, &bufferFrames, &tx_callback, (void *)&itdi);
+        dev.openStream(&parameters, NULL, RTAUDIO_SINT16, sampleRate, &bufferFrames, &tx_callback, (void *)&q);
         dev.startStream();
     }
     catch ( RtAudioError& e ) {
