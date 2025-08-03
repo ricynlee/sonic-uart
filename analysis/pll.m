58d3e2bb9a3dcb1df01971a42b297be096d78f77
ricynlee <ricyn@qq.com>
2025-08-03 22:25:23 +0800

    PLL: how to detect lock?

---
+++ analysis/pll.m
@@ -0,0 +1,86 @@
+clc;
+clear;
+
+%% stimulus params
+FS = 12e3; % sampling rate
+N = 65536;
+F = rand()*200-100; % freq offset
+PHI = 2*pi*rand(); % phase
+SNR =  -30;
+
+%% pll params
+LB = 128+16; % loop bandwidth in hz
+DF = sqrt(2)/2; % damping factor
+G = 1024+128; % gain
+
+init_beta = 2*DF*2*pi*LB/FS - (2*pi*LB)^2 / (FS^2*G);
+beta = init_beta; % proportional
+alpha = (2*pi*LB)^2 / (FS*G); % integral
+
+%% sim
+t = (0:N-1).'/FS;
+x = exp(1j*2*pi*F.*t+PHI) + wgn(N, 1, -SNR);% awgn(exp(1j*2*pi*F.*t+PHI), SNR, 'measured');
+y = ones(N, 1);
+f = zeros(N, 1);
+phi = zeros(N, 1);
+lock = false(N, 1);
+
+mav = 1024*ones(32, 1);
+
+for i = 1:N
+    delta = atan2(imag(x(i)/y(i)), real(x(i)/y(i))); % phase detector
+    if i<N
+        f(i+1) = f(i) + beta*delta;
+        phi(i+1) = phi(i) + alpha*delta + 2*pi*f(i+1)/FS;
+        while phi(i+1)>pi
+            phi(i+1) = phi(i+1)-2*pi;
+        end
+        while phi(i+1)<-pi
+            phi(i+1) = phi(i+1)+2*pi;
+        end
+    end
+    
+    if mod(i-1, 32)==0
+        mav = [delta; mav(1:31)];
+        if mav(32)~=1024
+            phidiff = max(mav)-min(mav);
+            if phidiff<2*pi
+                beta = beta*0.995;
+                f(i+1) = f(i) + beta*delta;
+            else
+                beta = beta*1.005;
+                if beta>init_beta; beta=init_beta; end
+                f(i+1) = f(i) + beta*delta;
+            end
+            if phidiff<pi
+                lock(i) = true;
+            end
+        end
+    else
+        lock(i) = lock(i-1);
+    end
+    
+    y(i+1) = exp(1j*phi(i));
+end
+
+%%
+clf;
+subplot(311);
+hold on;
+plot([0, t(N)], [F, F]);
+plot(t, f, 'r');
+plot(t, lock*200-100, 'g');
+hold off;
+ylim([-120,120]);
+grid on;
+if lock(end); title(sprintf('\\Delta f=%.3f',abs(f(end)-F))); end
+
+subplot(312);
+plot(t(1:1024), real(x(1:1024)));
+grid on;
+
+subplot(313);
+plot(t(1:1024), real(y(1:1024)));
+ylim([-1.2,1.2]);
+grid on;
+
